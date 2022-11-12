277cd22304d1e1727a3639e87ccb5e824969415a
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-11-12 15:09:50 +0100

    Added Tilemap, player and base animations
    added playerCharacter Sprite;
    added playerMovement Script;
    added PlayerBaseAnimations;
    added DungeonTileset;
    added Tilemap
    added Example Level;

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    bool facingRight = true;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(movement.x == -1) {
+            facingRight = false;
+        } else if(movement.x == 1)
+        {
+            facingRight = true;
+        }
+
+        transform.localScale = new Vector3((float)(movement.x != 0 ? movement.x * 0.25 : facingRight ? 0.25 : -0.25), transform.localScale.y, transform.localScale.z);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement     
+        rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * movement);
+    }
+}
