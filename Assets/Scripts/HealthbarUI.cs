ea8523bf2e587c60b224fda2c783611a50062e9c
4mierS <amiershr@gmail.com>
2022-11-24 08:13:17 +0100

    Update HealthSystem

--- Assets/Scripts/HealthbarUI.cs
+++ Assets/Scripts/HealthbarUI.cs
@@ -8,19 +8,39 @@ public class HealthbarUI : MonoBehaviour
 {
     public Transform TransformToFollow;
 
+    public GameObject ant;
+    private PlayerHealth health;
 
     private VisualElement m_Bar;
     private VisualElement[] Hearts;
-    private void Start()
+    void Start()
     {
+        //health = ant.GetComponent<PlayerHealth>();
+
         m_Bar = GetComponent<UIDocument>().rootVisualElement.Q("Container");
         Hearts = m_Bar.Children().ToArray();
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Hearts[i].style.visibility = Visibility.Hidden;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //if (health.curHealth < 3)
+        //{
+        //    Hearts[0].style.visibility = Visibility.Visible;
+        //}
+        //if (health.curHealth < 2)
+        //{
+        //    Hearts[1].style.visibility = Visibility.Visible;
+        //}
+        //if (health.curHealth < 1)
+        //{
+        //    Hearts[2].style.visibility = Visibility.Visible;
+        //}
     }
 
     public void AnimateBar(bool increaseHealth)
