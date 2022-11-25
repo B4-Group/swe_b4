d4fcc51448b990f35918aea63d3a05eb8bae78d7
4mierS <amiershr@gmail.com>
2022-11-25 19:16:03 +0100

    HeartBar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,36 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerHealth : MonoBehaviour
 {
     public static float curHealth;
-    public float maxHealth;
-
-    private HealthbarUI m_HealthbarUI;
-
+    public float maxHealth = 3;
+    
 
     private Rigidbody2D rb;
     private Animator anim;
+
+
+    private VisualElement m_Bar;
+    private VisualElement[] m_Hearts;
+
+    private VisualElement em_Bar;
+    private VisualElement[] em_Hearts;
     // Start is called before the first frame update
     void Start()
     {
         curHealth = maxHealth;
 
-        m_HealthbarUI = GetComponent<HealthbarUI>();
 
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        //Hearts
+        m_Bar = GetComponent<UIDocument>().rootVisualElement.Q("healthContainer");
+        m_Hearts = m_Bar.Children().ToArray();
+
+        em_Bar = GetComponent<UIDocument>().rootVisualElement.Q("emptyHealthContainer");
+        em_Hearts = em_Bar.Children().ToArray();
+
+        for (int loop = (int)maxHealth; loop > 0; loop--)
+        {
+            m_Hearts[loop - 1].style.visibility = Visibility.Visible;
+            em_Hearts[loop - 1].style.visibility = Visibility.Hidden;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Heartsbar: Num of Hearts is equal to curHealth
-     
-
-
         float hurt = anim.GetFloat("TakeDamage");
 
 
@@ -39,12 +53,27 @@ public class PlayerHealth : MonoBehaviour
         {
             anim.SetFloat("TakeDamage", hurt);
         }
+
+        if (curHealth == 2)
+        {
+            m_Hearts[0].style.visibility = Visibility.Hidden;
+            em_Hearts[0].style.visibility = Visibility.Visible;
+        }
+
+        if (curHealth == 1)
+        {
+            m_Hearts[1].style.visibility = Visibility.Hidden;
+            em_Hearts[1].style.visibility = Visibility.Visible;
+        }
+
         anim.SetFloat("TakeDamage", 0);
 
         //Set anim parameter "IsDead" on true if player has no life
         //Plays the Dying-Animation
         if (curHealth <= 0)
         {
+            m_Hearts[2].style.visibility = Visibility.Hidden;
+            em_Hearts[2].style.visibility = Visibility.Visible;
             Death();
         }
 
@@ -68,28 +97,5 @@ public class PlayerHealth : MonoBehaviour
         rb.bodyType = RigidbodyType2D.Static;
         anim.SetBool("IsDead", true);
     }
-
-    public void DamageShip()
-    {
-        if (curHealth > 0)
-        {
-            curHealth--;
-
-            m_HealthbarUI.AnimateBar(false);
-        }
-        if (curHealth == 0)
-        {
-            Invoke("SinkShip", 1);
-        }
-    }
-    public void HealShip()
-    {   
-        if (curHealth < 4)
-        {
-            curHealth++;
-            m_HealthbarUI.AnimateBar(true);
-        }
-    }
-
 }
 
