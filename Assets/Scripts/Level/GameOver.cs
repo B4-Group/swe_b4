d191afe816a0b93fabbbcec4207a322f2bf34992
Baran <Baran_ozbey02@outlook.de>
2022-11-29 19:06:34 +0100

    Added Game Over/Completed Screen

---
+++ Assets/Scripts/Level/GameOver.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class GameOver : MonoBehaviour
+{
+
+    public static bool gameOver = false;
+
+    private void DisableScreen()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        root.Q("GameOver").style.display = DisplayStyle.None;
+    }
+
+    private void Awake()
+    {
+        this.DisableScreen();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        UnityEngine.UIElements.Button neustart = root.Q<UnityEngine.UIElements.Button>("Neustart");
+        UnityEngine.UIElements.Button hauptmenu = root.Q<UnityEngine.UIElements.Button>("Hauptmenu");
+
+
+        hauptmenu.clicked += () => mainmenu();
+        neustart.clicked += () => restart();
+    }
+
+    private void mainmenu()
+    {
+        Pausenmenu obj = FindObjectOfType<Pausenmenu>();
+        obj.HauptMenu();
+
+        gameOver = false;
+    }
+    private void restart()
+    {
+        Pausenmenu obj = FindObjectOfType<Pausenmenu>();
+        obj.NeueStarten();
+        this.DisableScreen();
+        gameOver = false;
+    }
+
+    public void GameOverScreen()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        root.Q("GameOver").style.display = DisplayStyle.Flex;
+        Debug.Log("GameOver");
+        Time.timeScale = 0f;
+        gameOver = true;
+    }
+
+
+}
